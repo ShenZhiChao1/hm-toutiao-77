3e83d0e2a95c20a35c5ee1f3f2dd9f35e09263fa
zhichao <1291747281@qq.com>
2019-08-01 20:10:32 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
+import '@/style/index.less'
 // 把router组件挂在到main.js中
 import router from '@/router'
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,71 @@
 <template>
-    <div class="container">login</div>
+    <div class="container">
+        <el-card class="my-card">
+            <img src="../../assets/images/logo_index.png" alt="">
+            <el-input class="my-text" v-model="input" placeholder="请输入内容"></el-input>
+            <el-input class="my-code" placeholder="请输入验证码" v-model="input1" clearable></el-input>
+             <el-button type="info" class="codes">验证码</el-button>
+             <el-checkbox v-model="checked" class="yi">已同意</el-checkbox>
+             <el-button type="success" class="success">登录</el-button>
+        </el-card>
+    </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      input: '',
+      input1: '',
+      checked: ''
 
+    }
+  }
 }
 </script>
 
 <style scoped lang="less">
+.container{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: url(../../assets/images/login_bg.jpg) no-repeat center / cover
+
+}
+.my-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%);
+    img{
+        display: block;
+        width: 200px;
+        margin: 0 auto;
+    }
+    .my-text{
+
+       margin-top: 40px;
+
+    }
+    .my-code {
+        width:60%;
+        margin-top: 50px;
+        margin-bottom: 25px;
+    }
+    .codes {
+        font-size: 16px;
+        background-color: skyblue;
+        margin-left: 30px;
+
+    }
+    .success{
+        margin-left: 30px;
+        width: 80%;
+        margin-top: 10px;
+        font-size: 20px;
+    }
+}
 
 </style>
