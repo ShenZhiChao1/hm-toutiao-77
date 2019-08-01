234867767a6da3407249372e07f658c2b1b49db1
zhichao <1291747281@qq.com>
2019-08-01 16:11:41 +0800

    我是奥运冠军

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 把router组件挂在到main.js中
+import router from '@/router'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
+  router, // 记得在Vue.use中一定要加逗号
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/router.js
@@ -0,0 +1,9 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+// 导出router
+export default router
